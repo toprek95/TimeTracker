e760b23d00850390272ff4c2c12579ce785e3209
toprek95 <toprek.aleksandar@gmail.com>
2019-07-11 17:04:30 +0200

    Add domain models

---
+++ src/TimeTracker/Domain/TimeEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Domain
+{
+    public class TimeEntry
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public Project Project { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        [Range(1, 24)]
+        public int Hours { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal HourRate { get; set; }
+    }
+}
---
+++ src/TimeTracker/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Domain
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal HourRate { get; set; }
+    }
+}
