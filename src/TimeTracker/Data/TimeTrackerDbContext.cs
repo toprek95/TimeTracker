857909bf06c81020bd879846340be8d918317f3b
toprek95 <toprek.aleksandar@gmail.com>
2019-07-11 18:20:51 +0200

    Add EF Core and migrations

---
+++ src/TimeTracker/Data/TimeTrackerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.Domain;
+
+namespace TimeTracker.Data
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+        public TimeTrackerDbContext(DbContextOptions<TimeTrackerDbContext> options)
+            : base (options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+    }
+}
