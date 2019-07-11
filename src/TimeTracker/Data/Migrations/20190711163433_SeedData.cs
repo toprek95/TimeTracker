de04cc81829b0749970402629657e52dd6e6e684
toprek95 <toprek.aleksandar@gmail.com>
2019-07-11 18:41:02 +0200

    Add SeedData migration

---
+++ src/TimeTracker/Data/Migrations/20190711163433_SeedData.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeTracker.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Clients",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1L, "Client 1" });
+
+            migrationBuilder.InsertData(
+                table: "Clients",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2L, "Client 2" });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "Id", "ClientId", "Name" },
+                values: new object[] { 3L, 2L, "Project 3" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "HourRate", "Name" },
+                values: new object[] { 1L, 15m, "Aleksandar" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "HourRate", "Name" },
+                values: new object[] { 2L, 20m, "Tanja" });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "Id", "ClientId", "Name" },
+                values: new object[] { 1L, 1L, "Project 1" });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "Id", "ClientId", "Name" },
+                values: new object[] { 2L, 1L, "Project 2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 2L);
+        }
+    }
+}
--- src/TimeTracker/Data/TimeTrackerDbContext.cs
+++ src/TimeTracker/Data/TimeTrackerDbContext.cs
@@ -17,5 +17,23 @@ namespace TimeTracker.Data
         public DbSet<Project> Projects { get; set; }
 
         public DbSet<TimeEntry> TimeEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Aleksandar", HourRate = 15m },
+                new User { Id = 2, Name = "Tanja", HourRate = 20m });
+
+            modelBuilder.Entity<Client>().HasData(
+                new Client { Id = 1, Name = "Client 1"},
+                new Client { Id = 2, Name = "Client 2"});
+
+            modelBuilder.Entity<Project>().HasData(
+                new { Id = 1L, Name = "Project 1", ClientId = 1L},
+                new { Id = 2L, Name = "Project 2", ClientId = 1L},
+                new { Id = 3L, Name = "Project 3", ClientId = 2L});
+        }
     }
 }
