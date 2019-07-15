7f64e15bafc9cdd372523e482b6d60c0fffa914d
toprek95 <toprek.aleksandar@gmail.com>
2019-07-15 17:24:05 +0200

    Add clients, projects and time entries controllers

--- src/TimeTracker/Models/ProjectInputModel.cs
+++ src/TimeTracker/Models/ProjectInputModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using TimeTracker.Domain;
 
 namespace TimeTracker.Models
 {
@@ -9,5 +6,10 @@ namespace TimeTracker.Models
     {
         public string Name { get; set; }
         public long ClientId { get; set; }
+
+        public void MapTo(Project project)
+        {
+            project.Name = Name;
+        }
     }
 }
--- src/TimeTracker/Models/TimeEntryInputModel.cs
+++ src/TimeTracker/Models/TimeEntryInputModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TimeTracker.Domain;
 
 namespace TimeTracker.Models
 {
@@ -12,5 +13,12 @@ namespace TimeTracker.Models
         public DateTime EntryDate { get; set; }
         public int Hours { get; set; }
         public string Description { get; set; }
+
+        public void MapTo (TimeEntry timeEntry)
+        {
+            timeEntry.EntryDate = EntryDate;
+            timeEntry.Hours = Hours;
+            timeEntry.Description = Description;
+        }
     }
 }
