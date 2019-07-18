e9d526b70786aa4dd9dff4ea272307755f975249
toprek95 <toprek.aleksandar@gmail.com>
2019-07-18 17:25:53 +0200

    Add auth infrastucture

---
+++ src/TimeTracker.Client/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Client.Models
+{
+    public class UserModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal HourRate { get; set; }
+    }
+}
--- src/TimeTracker.Client/Startup.cs
+++ src/TimeTracker.Client/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using TimeTracker.Client.Security;
 
 namespace TimeTracker.Client
 {
@@ -7,6 +8,8 @@ namespace TimeTracker.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorizationCore();
+            services.AddTokenAuthenticationStateProvider();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
